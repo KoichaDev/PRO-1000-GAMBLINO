8ed3fa55f0132e4f929f213c2fa78fa85125acb8
“Khoi <khoi@godtsagt.no>
2022-03-09 22:53:19 +0100

    Update: ✨ Renamed the mu-plugins block to gamblino-blocks instead

--- wp-content/mu-plugins/index.php
+++ wp-content/mu-plugins/index.php
@@ -7,18 +7,6 @@ Author: Khoi Hoang
 Author URI: https://gamblino.com
 */
 
-
-// $custom_post_type_files = plugin_dir_path(__FILE__) . 'block-*';
-
-// function auto_load_files_from_folder($folder_path = '') {
-//     // autoload every files a folder
-//     foreach (glob($folder_path) as $file_name) {
-//         require $file_name;
-//     }
-// }
-
-// auto_load_files_from_folder($custom_post_type_files);
-
-include_once('block-gamblino-info-bonus-review/loader.php'); 
+include_once('gamblino-blocks/loader.php'); 
 
 
