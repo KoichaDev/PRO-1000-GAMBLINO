e8dc468f10b4b9ba39b7746c31ff40608b637003
“Khoi <khoi@godtsagt.no>
2022-03-29 21:52:46 +0200

    Add: ✨ Align wide and full theme support for default native gutenberg blocks
    Next is to add the custom block

--- wp-content/themes/gamblino/inc/functions/editor/block_editor_theme_setup.php
+++ wp-content/themes/gamblino/inc/functions/editor/block_editor_theme_setup.php
@@ -2,7 +2,10 @@
 
 function gamblino_block_editor_theme_setup() {
 	add_theme_support( 'editor-styles' );
-	add_editor_style( 'style-editor.css' ); 
+	add_editor_style( 'style-editor.css' );
+	
+	
+	add_theme_support( 'align-wide');
 }
 
 add_action( 'after_setup_theme', 'gamblino_block_editor_theme_setup' );
