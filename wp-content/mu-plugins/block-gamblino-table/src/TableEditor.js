de31a3cc81c2ebb953f7b01b20f10e33148a65e5
Khoi Hoang <khoii87@gmail.com>
2022-04-06 21:40:41 +0200

    Add: ✨ TableHead component

--- wp-content/mu-plugins/block-gamblino-table/src/TableEditor.js
+++ wp-content/mu-plugins/block-gamblino-table/src/TableEditor.js
@@ -24,6 +24,7 @@ import {
 
 // Table Components
 import TableCaption from './components/TableCaption';
+import TableHead from './components/TableHead'
 import TableBody from './components/TableBody';
 
 
@@ -51,51 +52,6 @@ const TableEditor = (props) => {
 
     // Row Counter for aria labels - start at 1
     let ariaLabel = '';
-    let rowCounter = 1;
-
-    // Table Head
-    let tableHead = '';
-    let headClass = 'is-hidden';
-
-    if (showTable) {
-        headClass = '';
-    }
-    const tableHeadData = dataHead.map((cell, colIndex) => {
-        ariaLabel = 'Row ' + rowCounter + ' Column ' + (colIndex + 1);
-        let currentThButtons = '1,2,4,5';
-        // If row headings are enabled, and this is the very first col TH, don't allow insert col before or delete col
-        if (colIndex == 0 && useRowHeadings == true) {
-            currentThButtons = '2,4';
-        }
-        let currentTh = <th
-            aria-label={ariaLabel}
-            scope='col'
-            contenteditable='true'
-            data-buttons={currentThButtons}
-            onFocus={evt => enterCellState(evt, props)}
-        >
-            {cell.content}
-        </th>;
-        currentTh.props.onInput = function (evt) {
-            // Copy the dataHead
-            let newHead = JSON.parse(JSON.stringify(dataHead));
-            // Create a new cell
-            let newTh = { content: evt.target.textContent };
-            // Replace the old cell with the new cell
-            newHead.splice(colIndex, 1, newTh);
-            // Save the dataHead attribute
-            props.setAttributes({ dataHead: newHead });
-            // Move the cursor back where it was
-            setCursor(evt);
-        };
-        return currentTh;
-    });
-    if (tableHeadData.length) {
-        tableHead = <thead className={headClass}><tr>{tableHeadData}</tr></thead>;
-    } else {
-        // If there is no table head, take rowCounter back down to 0, because Table Body has to increment it before output
-        rowCounter--;
-    }
 
     // let formClass = '';
 
@@ -151,7 +107,7 @@ const TableEditor = (props) => {
 
             <table className={className}>
                 <TableCaption  {...props} />
-                {tableHead}
+                <TableHead {...props} />
                 <TableBody formClass={formClass} setFormClass={setFormClass} {...props} />
                 {tableFooter}
             </table>
---
+++ wp-content/mu-plugins/block-gamblino-table/src/components/TableHead.js
@@ -0,0 +1,63 @@
+import { enterCellState, setCursor } from '../hooks/useTable'
+
+const TableHead = ({ ...props }) => {
+    const { attributes, setAttributes } = props
+    const { dataHead, showTable, useRowHeadings } = attributes
+
+    // Table Head
+    let tableHead = '';
+    let headClass = 'is-hidden';
+
+    let rowCounter = 1;
+
+    if (showTable) {
+        headClass = '';
+    }
+
+    const onInputHandler = (evt) => {
+        // Copy the dataHead
+        let newHead = JSON.parse(JSON.stringify(dataHead));
+        // Create a new cell
+        let newTh = { content: evt.target.textContent };
+        // Replace the old cell with the new cell
+        newHead.splice(colIndex, 1, newTh);
+        // Save the dataHead attribute
+        setAttributes({ dataHead: newHead });
+        // Move the cursor back where it was
+        setCursor(evt);
+    };
+
+
+
+
+    const tableHeadData = dataHead.map((cell, colIndex) => {
+        const ariaLabel = 'Row ' + rowCounter + ' Column ' + (colIndex + 1);
+        let currentThButtons = '1,2,4,5';
+        // If row headings are enabled, and this is the very first col TH, don't allow insert col before or delete col
+        if (colIndex == 0 && useRowHeadings == true) {
+            currentThButtons = '2,4';
+        }
+        let currentTh = <th
+            aria-label={ariaLabel}
+            scope='col'
+            contenteditable='true'
+            data-buttons={currentThButtons}
+            onFocus={evt => enterCellState(evt, props)}
+            onInput={onInputHandler}
+        >
+            {cell.content}
+        </th>;
+
+        return currentTh;
+    });
+    if (tableHeadData.length > 0) {
+        tableHead = <thead className={headClass}><tr>{tableHeadData}</tr></thead>;
+    } else {
+        // If there is no table head, take rowCounter back down to 0, because Table Body has to increment it before output
+        rowCounter--;
+    }
+
+    return <>{tableHead}</>
+}
+
+export default TableHead
