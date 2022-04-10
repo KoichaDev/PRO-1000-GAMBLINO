94acbe5fdde3352e53b9d8f531b581e203b3ad21
Khoi Hoang <khoii87@gmail.com>
2022-04-10 14:52:07 +0200

    Refactor: ♻️ Better readability for the table content of th vs td element

--- wp-content/mu-plugins/block-gamblino-table/src/components/TableEditor/TableEditorBody.js
+++ wp-content/mu-plugins/block-gamblino-table/src/components/TableEditor/TableEditorBody.js
@@ -1,6 +1,3 @@
-// WordPress dependencies
-import { createElement } from '@wordpress/element'
-
 // hooks
 import { setCursor } from '../../hooks/useTable'
 import { enterCellState } from '../../hooks/useTableCells'
@@ -9,70 +6,66 @@ const TableEditorBody = ({ rowCounter, ...props }) => {
     const { setIsHiddenClassName, attributes, setAttributes } = props;
     const { textAlignment, dataBody, useRowHeadings } = attributes;
 
-    let tableBodyContent = '';
-    let ariaLabel = '';
-
     const onInputHandler = (evt, rowIndex, colIndex) => {
-        // Copy the dataBody
         const newBody = [...dataBody];
+
         // Create a new cell
-        const newCell = {
-            content: evt.target.textContent
-        };
+        const newCell = { content: evt.target.textContent};
 
         // Replace the old cell
         newBody[rowIndex].bodyCells[colIndex] = newCell;
 
-
-        // Set the attribute
-        setAttributes({
-            dataBody: newBody,
-        });
+        setAttributes({ dataBody: newBody, });
         // Move the cursor back where it was
         setCursor(evt);
     }
-
 
     const tableBodyData = dataBody.map((rows, rowIndex) => {
         rowCounter++;
 
         const rowCells = rows.bodyCells.map((cell, colIndex) => {
             // Set up options
-            ariaLabel = 'Row ' + rowCounter + ' Column ' + (colIndex + 1);
-            let cellType = 'd';
+            let ariaLabel = `Row ${rowCounter} Column ${colIndex + 1}`;
 
-            const cellOptions = {
-                'aria-label': ariaLabel,
-                contenteditable: 'true',
-                'data-buttons': '1,2,3,4,5,6',
-                class: `text-align-${textAlignment}`,
-                onFocus: (evt) => enterCellState(evt, props),
-                onInput: (evt) => onInputHandler(evt, rowIndex, colIndex)
-            };
-
-            if (useRowHeadings == true && colIndex == 0) {
-                cellType = 'h';
-                cellOptions['data-buttons'] = '2,3,4,6';
-                cellOptions.scope = 'row';
+            if (useRowHeadings === true && colIndex == 0) {
+                return (
+                    <th
+                        aria-label={ariaLabel}
+                        contentEditable="true"
+                        data-buttons="2,3,4,6"
+                        scope='row'
+                        class={`text-align-${textAlignment}`}
+                        onFocus={(evt) => enterCellState(evt, props)}
+                        onInput={(evt) => onInputHandler(evt, rowIndex, colIndex)}
+                    >{cell.content}</th>
+                )
             }
-            // Create the element - either a TD or a TH
-            const currentBodyCell = createElement(
-                `t${cellType}`,
-                cellOptions,
-                cell.content
-            )
 
-            return currentBodyCell;
+            return (
+                <td
+                    aria-label={ariaLabel}
+                    contentEditable="true"
+                    data-buttons="1,2,3,4,5,6"
+                    class={`text-align-${textAlignment}`}
+                    onFocus={(evt) => enterCellState(evt, props)}
+                    onInput={(evt) => onInputHandler(evt, rowIndex, colIndex)}
+                >{cell.content}</td>
+            )
         });
         return (<tr>{rowCells}</tr>);
     });
 
     if (tableBodyData.length) {
-        tableBodyContent = <tbody>{tableBodyData}</tbody>;
         setIsHiddenClassName('is-hidden');
-        // formClass = 'is-hidden';
     }
-    return <>{tableBodyContent}</>;
+    
+    return (
+        <>
+            {tableBodyData.length > 0 && (
+                <tbody>{tableBodyData}</tbody>
+            )}
+        </>
+    );
 }
 
 export default TableEditorBody
