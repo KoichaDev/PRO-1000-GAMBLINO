3212dbae8544d0c5d8a59132cf94abcc00c576d5
Khoi Hoang <khoii87@gmail.com>
2022-04-10 23:42:59 +0200

    Remove: 🔥Custom post type reviews

--- wp-content/themes/gamblino/inc/functions/custom_post_types/cpt_admin_sub_menu.php
+++ wp-content/themes/gamblino/inc/functions/custom_post_types/cpt_admin_sub_menu.php
@@ -25,15 +25,6 @@
         add_submenu_page(
 		    'page', // Parent slug
 		    'subpage', // Page title
-		    'Reviews', // Menu title
-		    'manage_options', // Capability
-		    'edit.php?post_type=reviews',  // Slug
-		    false // Function
-		);
-        
-        add_submenu_page(
-		    'page', // Parent slug
-		    'subpage', // Page title
 		    'Casino Reviews', // Menu title
 		    'manage_options', // Capability
 		    'edit.php?post_type=casino-reviews',  // Slug
