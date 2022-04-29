8f016940d20a9fffcf0fe5a209c3a6518003fa67
Khoi Hoang <khoii87@gmail.com>
2022-04-29 22:45:01 +0200

    Add: ✨ Partial refresh for site identify "Site Title"

--- wp-content/themes/gamblino/inc/functions/theme_customizer/Site_Identify.php
+++ wp-content/themes/gamblino/inc/functions/theme_customizer/Site_Identify.php
@@ -7,6 +7,16 @@ class Site_Identify {
 
     public static function gamblino_theme_customize_site_identidy( $wp_customize ) {
         $wp_customize -> get_setting('blogname') -> transport = 'postMessage';
+
+        // Partial refresh only refreshing a setion of a place on the page instead of whole website
+        // This will make it more performant for the page
+        $wp_customize -> selective_refresh -> add_partial('blogname', [
+            'selector'              => '#header-blogname',
+            'container_inclusive'   => false,
+            'render_callback'       => function() {
+                bloginfo( 'name' );
+            } 
+        ]);
     }
 }
 
