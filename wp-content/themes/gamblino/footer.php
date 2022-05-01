a2e13cec928ac92a12ef68d4d01565cf9a7ed15a
“Khoi <khoi@godtsagt.no>
2022-05-01 03:19:01 +0200

    WIP: 🚧 need to refacto the class of ACF Social Media Icons to be a private stuff

--- wp-content/themes/gamblino/footer.php
+++ wp-content/themes/gamblino/footer.php
@@ -1,5 +1,7 @@
 <?php
-    include_once 'inc/functions/advanced_custom_fields/ACF_Footer_Flexible_Content.php';
+    include_once 'inc/functions/advanced_custom_fields/Footer/ACF_Footer_Flexible_Content.php';
+    include_once 'inc/functions/advanced_custom_fields/Footer/ACF_Footer_Social_Media_Content.php';
+
     $site_info = get_theme_mod( 'gamblino_footer_site_info' );
     $footer_bg_color = get_theme_mod( 'gamblino_background_color_footer' );
     wp_footer();
@@ -14,7 +16,12 @@
                 ],
             ];
 
-    $global_footer = new ACF_Footer_Flexible_Content( 'global_footer_flexible_content', 'global_footer_menu' );
+    $global_footer_menu = new ACF_Footer_Flexible_Content( 'global_footer_flexible_content', 'global_footer_menu' );
+    $global_footer_social_media_icons = new ACF_Footer_Social_Media_Content();
+
+    var_dump($global_footer_social_media_icons -> get_social_media_groups());
+
+
 ?>
 
 <footer class="[ footer-main ]" style="background-color: <?= $footer_bg_color; ?>">
--- wp-content/themes/gamblino/inc/functions/advanced_custom_fields/Footer/ACF_Footer_Social_Media_Content.php
+++ wp-content/themes/gamblino/inc/functions/advanced_custom_fields/Footer/ACF_Footer_Social_Media_Content.php
@@ -1,5 +1,34 @@
 <?php
 
 class ACF_Footer_Social_Media_Content {
-    
+    public $field_name = 'global_footer_social_media_repeater_field';
+    public $social_media_groups = [];
+
+    // private $fields = get_fields('global_footer_social_media_repeater_field', 'options');
+
+    public function __construct( ) {
+        $this -> get_social_media_repeater_field_content( $this -> field_name );
+    }
+
+    public function get_social_media_repeater_field_content( $field_name) {        
+        if( !have_rows($field_name, 'options' ) ) return;
+        while( have_rows($field_name, 'options' ) ) : the_row();
+
+            $social_media_groups = [
+                'media' => get_sub_field('image_group'),
+                'accessibility_info' => get_sub_field('accessibility_group'),
+                'no_follow' => get_sub_field('html_attr_no_follow'),
+            ];
+
+           $this -> set_social_media_content( $social_media_groups );
+        endwhile;
+    }
+
+    public function set_social_media_content( $value ) {
+        $this -> social_media_groups[] = $value;
+    }
+
+    public function get_social_media_groups() {
+        return $this -> social_media_groups;
+    }
 }
