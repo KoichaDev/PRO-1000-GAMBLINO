51d6e13f698ab56d67032358071961c6c7648366
“Khoi <khoi@godtsagt.no>
2022-05-08 15:11:37 +0200

    refactor: ♻️ category Items to Categories menu lives own dedicated part folder

--- wp-content/themes/gamblino/views/navbar/mobile-categories.view.php
+++ wp-content/themes/gamblino/views/navbar/mobile-categories.view.php
@@ -5,10 +5,8 @@
     $headerBackgroundColor = $NavbarMenuColorOptions -> getBackgroundColor();
     $titleTextColor = $NavbarMenuColorOptions -> getTitleTextColor();
 
-    $navbarMenuCategoryListOption = new NavbarMenuCategoryListOption();
-    
-    $categoryTitle = $navbarMenuCategoryListOption -> getCategoryTitle();
-    $categoryItems = $navbarMenuCategoryListOption -> getCategoryItems();
+    $categoryTitle = (new NavbarMenuCategoryListOption()) -> getCategoryTitle();
+
    
 ?>
 
@@ -40,37 +38,7 @@
     <p class="text-xs text-uppercase" style="color: <?= $titleTextColor; ?>">
         <?= $categoryTitle; ?>
     </p>
-    <div class="[ navbar-phone-menu__container ] [ flex-row flex-wrap justify-content-between align-items-center my-5 ]">
-        <?
-            foreach ($categoryItems as $categoryItem) :
+    
+    <?php include(get_template_directory() . '/views/navbar/parts/mobile-category-menu.php'); ?>
 
-            $categoryTitle = $categoryItem['general_html_content_group']['category_title'];
-            $isFollowHTMLAttr = $categoryItem['general_html_content_group']['html_attr_no_follow'];
-
-            $imageId = $categoryItem['image_group']['image']['id'];
-            $imageUrl = $categoryItem['image_group']['url'];
-            $imageAltText = $categoryItem['image_group']['alt_text'];
-
-            $ariaLabel = $categoryItem['accessibility_group']['aria_label'];
-            $title = $categoryItem['accessibility_group']['title'];
-        ?>
-            <a 
-                href="<?= $imageUrl?>" 
-                role="button"
-                class="[ card-category ] [ flex-column flex-wrap justify-content-center align-items-center text-xs text-center text-no-underline ]"
-                style="color: <?= $titleTextColor; ?>"
-                aria-label="<?= $ariaLabel; ?>"
-                title="<?= $title; ?>"
-            >
-            <?php
-                echo wp_get_attachment_image($imageId, '25x25', false, [
-                    'class' => 'card-category__image',
-                    'alt' => $imageAltText,
-                ]);
-
-                echo $categoryTitle;
-            ?>
-            </a>
-        <?php endforeach; ?>
-    </div>
 </nav>
---
+++ wp-content/themes/gamblino/views/navbar/parts/mobile-category-menu.php
@@ -0,0 +1,37 @@
+<?php
+    $categoryItems = (new NavbarMenuCategoryListOption()) -> getCategoryItems();
+?>
+
+<div class="[ navbar-phone-menu__container ] [ flex-row flex-wrap justify-content-between align-items-center my-5 ]">
+    <?
+        foreach ($categoryItems as $categoryItem) :
+
+        $categoryTitle = $categoryItem['general_html_content_group']['category_title'];
+        $isFollowHTMLAttr = $categoryItem['general_html_content_group']['html_attr_no_follow'];
+
+        $imageId = $categoryItem['image_group']['image']['id'];
+        $imageUrl = $categoryItem['image_group']['url'];
+        $imageAltText = $categoryItem['image_group']['alt_text'];
+
+        $ariaLabel = $categoryItem['accessibility_group']['aria_label'];
+        $title = $categoryItem['accessibility_group']['title'];
+    ?>
+        <a 
+            href="<?= $imageUrl?>" 
+            role="button"
+            class="[ card-category ] [ flex-column flex-wrap justify-content-center align-items-center text-xs text-center text-no-underline ]"
+            style="color: <?= $titleTextColor; ?>"
+            aria-label="<?= $ariaLabel; ?>"
+            title="<?= $title; ?>"
+        >
+        <?php
+            echo wp_get_attachment_image($imageId, '25x25', false, [
+                'class' => 'card-category__image',
+                'alt' => $imageAltText,
+            ]);
+
+            echo $categoryTitle;
+        ?>
+        </a>
+    <?php endforeach; ?>
+</div>
