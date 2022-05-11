3d2cf9eb9fd27ac8b7a3cdc8b190d98b95e8d7a8
ᕕ( ᐛ )ᕗ <khoi@godtsagt.no>
2022-05-11 21:10:03 +0200

    WIP: try to auto instnatiate the object

--- wp-content/themes/gamblino/lib/acf/blocks/Loader.php
+++ wp-content/themes/gamblino/lib/acf/blocks/Loader.php
@@ -1,3 +1,12 @@
 <?php 
 
 new CarouselConfigBlock();
+
+
+foreach (glob(get_theme_file_path() . '/lib/acf/blocks/**/*.php') as $filename) {
+
+    $str = 'CarouselConfigBlock.php';
+    include $filename;
+    $filename = basename($filename, '.php');
+    new $filename();
+}
