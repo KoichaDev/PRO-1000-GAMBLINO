d6abb9088d0f1c21e6f17f0e56efc8b49fe29de8
Khoi Hoang <khoii87@gmail.com>
2022-05-29 15:06:06 +0200

    feature: ✨ storing the image

--- wp-content/mu-plugins/gamblino-blocks/src/blocks/BlockRankingCard/components/Image/Image.component.js
+++ wp-content/mu-plugins/gamblino-blocks/src/blocks/BlockRankingCard/components/Image/Image.component.js
@@ -1,20 +1,48 @@
 import { __ } from "@wordpress/i18n";
 
 import { MediaPlaceholder } from "@wordpress/block-editor";
+import { isBlobURL } from "@wordpress/blob";
+import { Spinner } from "@wordpress/components";
 
 import { BsImage } from "react-icons/bs";
 
 const ImageEdit = ({ attributes, setAttributes }) => {
+    const { url, alt } = attributes;
     console.log(attributes);
+    const onSelectImageHandler = (value) => {
+        if (!value || !value.url) {
+            setAttributes({
+                id: undefined,
+                url: undefined,
+                alt: "",
+            });
+        }
+
+        setAttributes({
+            id: value.id,
+            url: value.url,
+            alt: value.alt,
+        });
+    };
+
     return (
         <>
+            {url && (
+                <div className={`[ media-image ] ${isBlobURL(url) ? " [ is-loading ]" : ""}`}>
+                    <img src={url} alt={alt} />
+
+                    {isBlobURL(url) && <Spinner />}
+                </div>
+            )}
+
             <MediaPlaceholder
                 icon={BsImage}
-                onSelect={(value) => console.log(value)}
+                onSelect={onSelectImageHandler}
                 onSelectURL={(value) => console.log(value)}
                 onError={(value) => console.log(value)}
                 accept="image/*"
                 allowedTypes={["image"]}
+                disableMediaButtons={url ? true : false}
             />
         </>
     );
