47e5fb56dee2477b3feb00fe16a6d2b91939cf64
Khoi Hoang <khoii87@gmail.com>
2022-06-09 01:39:03 +0200

    feature: ✨ Button icon

---
+++ wp-content/mu-plugins/gamblino-blocks/src/blocks/common/BlockButton/index.js
@@ -0,0 +1,25 @@
+// WP block components
+import { registerBlockType } from "@wordpress/blocks";
+
+// Button Icon
+import ButtonIcon from './icon/ButtonIcon';
+
+// Button Component
+import EditBlockButton from "./EditBlockButton";
+import SaveBlockButton from "./SaveBlockButton";
+
+// block meta data
+import json from './block.json'
+
+// WP Block styles
+import "./style.scss";
+
+const { name, icon, ...settings } = json
+
+registerBlockType(name, {
+    ...settings,
+    icon: ButtonIcon,
+    edit: EditBlockButton,
+    save: SaveBlockButton,
+});
+
