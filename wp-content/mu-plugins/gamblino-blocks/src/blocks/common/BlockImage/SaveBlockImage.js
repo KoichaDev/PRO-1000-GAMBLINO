d95b327a2ac0ef7a0ce3d03528616a505715a31a
Khoi Hoang <khoii87@gmail.com>
2022-06-11 04:22:58 +0200

    feature: ✨ Image Block Save function is corrected for the styling

--- wp-content/mu-plugins/gamblino-blocks/src/blocks/common/BlockImage/SaveBlockImage.js
+++ wp-content/mu-plugins/gamblino-blocks/src/blocks/common/BlockImage/SaveBlockImage.js
@@ -1,12 +1,48 @@
 import { useBlockProps } from "@wordpress/block-editor";
 
 const SaveBlockImage = ({ attributes }) => {
-    const { url, id, alt } = attributes;
+    const {
+        id,
+        url,
+        alt,
+        imageDimension,
+        positionType,
+        positionValue,
+        margin,
+        marginUnit,
+        marginAuto,
+        isResetMargin,
+    } = attributes;
+
+    // prettier-ignore
+    const positionStyle = positionType === "absolute" && {
+        top: `${positionValue.top}%`,
+        right: `${positionValue.right}%`,
+        bottom: `${positionValue.bottom}%`,
+        left: `${positionValue.left}%`,
+    };
+
+    // prettier-ignore
+    const marginStyle = isResetMargin === true ? marginAuto : {
+        marginTop: `${margin.top}${marginUnit}`,
+        marginRight: `${margin.right}${marginUnit}`,
+        marginBottom: `${margin.bottom}${marginUnit}`,
+        marginLeft: `${margin.left}${marginUnit}`,
+    };
 
     const className = id ? `wp-image-${id}` : null;
 
     return (
-        <div {...useBlockProps.save()}>
+        <div
+            {...useBlockProps.save({
+                style: {
+                    width: imageDimension,
+                    position: positionType,
+                    ...positionStyle,
+                    ...marginStyle,
+                },
+            })}
+        >
             {url && <img src={url} className={className} alt={alt} />}
         </div>
     );
