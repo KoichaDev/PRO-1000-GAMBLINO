77b51840d9f019378d2771fadcf302306c8d8832
Khoi Hoang <khoii87@gmail.com>
2022-06-17 02:46:09 +0200

    add: ✨ Icon for Block Input Review

--- wp-content/mu-plugins/gamblino-blocks/src/blocks/BlockInsertReview/index.js
+++ wp-content/mu-plugins/gamblino-blocks/src/blocks/BlockInsertReview/index.js
@@ -5,19 +5,20 @@ import { registerBlockType } from "@wordpress/blocks";
 import Edit from "./Edit";
 import Save from "./Save";
 
-import './components/InnerBlocks/RegisterBlockType';
+import "./components/InnerBlocks/RegisterBlockType";
 
+import { AiOutlineCrown } from "react-icons/ai";
 // block meta data
-import json from './block.json'
+import json from "./block.json";
 
 // WP Block styles
 import "./style.scss";
 
-const { name, ...settings } = json
+const { name, icon, ...settings } = json;
 
 registerBlockType(name, {
 	...settings,
+	icon: AiOutlineCrown,
 	edit: Edit,
 	save: Save,
 });
-
